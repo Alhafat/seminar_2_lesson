0837dd3a1c8e0ee4510d8758f7665705bd14b36b
Alhafat <vladimir-lukin@inbox.ru>
2023-01-17 21:16:47 +0300

    exp_002

--- exp_002/Program.cs
+++ exp_002/Program.cs
@@ -6,7 +6,7 @@ Console.Clear();
 int number=new Random().Next(100,1000); // всегда не берет последнее число, поэтому указываем +1
 Console.WriteLine(number);
 int a1 =number/100; // целочисленное деление берем только левую цифру
-int a2 =number%10;
+int a3 =number%10;
 
 int result=a1*10+a2;
 
