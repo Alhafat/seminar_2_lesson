834cdf9bc5f32d984af53457795481aa7bd07048
Alhafat <vladimir-lukin@inbox.ru>
2023-01-17 22:17:46 +0300

    exp_005

--- exp_005/Program.cs
+++ exp_005/Program.cs
@@ -5,25 +5,37 @@
 // ● 25, 5 -> да 
 // ● 8,9 -> нет
 
+Console.Clear();
 Console.Write("Введите первое число: ");
 int number1=int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int number2=int.Parse(Console.ReadLine()!);
 
-int result=number2*number2;
+// //вуариант 1
+// int result=number2*number2;
 
-if (number1-number2*number2==0)
+// if (number1-number2*number2==0)
+// {
+//     Console.WriteLine($"Квадрат числа {number2} равен введенному числу {number1}");
+// }
+// else
+// {
+//     if (number2-number1*number1==0)
+//     {
+//         Console.WriteLine($"Квадрат числа {number1} равен введенномучислу {number2}");
+//     }
+//     else
+//     {
+//     Console.WriteLine($"Ни одно введенное число не является квадратом другого");
+//     }
+// }
+
+//вариант 2
+if ((number1==number2*number2)||(number2==number1*number1))      
 {
-    Console.WriteLine($"Квадрат числа {number2} равен введенному числу {number1}");
+    Console.WriteLine($"Одно из введенных чисел является квадратом другого");
 }
 else
 {
-    if (number2-number1*number1==0)
-    {
-        Console.WriteLine($"Квадрат числа {number1} равен введенномучислу {number2}");
-    }
-    else
-    {
-    Console.WriteLine($"Ни одно введенное число не является квадратом другого");
-    }
+    Console.WriteLine($"Ни одно веденноое число не является квадратом другого");
 }
